6fa943616e491df5e355223056961df09521cb75
João Victor Rodrigues <jvpr123@outlook.com>
2022-05-24 11:02:02 -0300

    feat: implement data-layer
    data-layer: implement AddAccount interface defined on domain-layer; integrate password encrypter

---
+++ src/data/useCases/addAccount/DbAddAccount.ts
@@ -0,0 +1,19 @@
+import { AccountModel } from '../../../domain/models/Account.model';
+import { IAddAccount, IAddAccountModel } from '../../../domain/useCases/AddAccount.usecase'
+import { IAddAccountRepository } from '../../protocols/addAccountRepository.interface';
+import { IEncrypter } from '../../protocols/encrypter.interface';
+
+export class DbAddAccount implements IAddAccount {
+    constructor (
+        private readonly encrypter: IEncrypter,
+        private readonly addAccountRepository: IAddAccountRepository,
+    ) {}
+
+    async add (account: IAddAccountModel): Promise<AccountModel> {
+        const hashedPassword = await this.encrypter.encrypt(account.password)
+        const createdAccount = await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
+
+        return new Promise(resolve => resolve(createdAccount))
+    }
+
+}
