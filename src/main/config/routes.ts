9bf26211c7b42dbb285299b632595f5149a02249
João Victor P. Rodrigues <jvpr123@outlook.com>
2022-05-31 11:36:48 -0300

    build: adjust files to build succeeds

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,17 @@
 import { Express, Router } from "express"
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
     const router = Router()
     app.use('/api', router)
 
-    fg.sync('**/src/main/routes/**.routes.ts').map(async (file) => {
-        (await import(`../../../${file}`)).default(router)
+    // fg.sync('**/src/main/routes/**.routes.ts').map(async (file) => {
+    //     (await import(`../../../${file}`)).default(router)
+    // })
+
+    readdirSync(`${__dirname}/../../main/routes`).map(async (file) => {
+        if (!file.includes('.test.ts')) {
+            (await import(`../routes/${file}`)).default(router)
+        }
     })
 }
