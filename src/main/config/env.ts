9839d78c45d665c1e1dba5e265c7db06392e2368
João Victor Rodrigues <jvpr123@outlook.com>
2022-05-31 17:04:02 -0300

    feat: add Dockerfile and docker-compose to run api and database as containerized services

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://labluby:clean-node-api@clean-node-api.xqmqodn.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:27017/clean-node-api',
     port: process.env.PORT || 3030,
     bcryptSalt: process.env.BCRYPT_SALT || 12,
     jwtSecret: process.env.JWT_SECRET || 'secret_key',
